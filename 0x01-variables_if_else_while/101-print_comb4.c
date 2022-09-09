358efcb2d9417795dc891686691c0d354e2b5dee
Nwajei edgar <edgarlinked@gmail.com>
2022-09-09 11:37:35 -0700

    Success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,13 @@ int main(void)
 	for (var = 0; var < 8; var++)
 	{
 		int ii;
-
-		for (jj = ii + 1; jj < 10; jj++)
+		
+		for (ii = var + 1; ii < 9; ii++)
 		{
+			int jj;
+
+			for (jj = ii + 1; jj < 10; jj++)
+			{
 			putchar((var % 10) + '0');
 			putchar((ii % 10) + '0');
 			putchar((jj % 10) + '0');
