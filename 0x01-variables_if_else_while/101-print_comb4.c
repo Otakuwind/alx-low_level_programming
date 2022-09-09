fd6178380235bcfcf1fe57ceb734ce461b6dd9de
Nwajei edgar <edgarlinked@gmail.com>
2022-09-09 11:30:37 -0700

    success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - output all single number of base 10
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 8; var++)
+	{
+		int ii;
+
+		for (jj = ii + 1; jj < 10; jj++)
+		{
+			putchar((var % 10) + '0');
+			putchar((ii % 10) + '0');
+			putchar((jj % 10) + '0');
+
+			if (var != 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+}
+
+putchar('\n');
+
+return (0);
+}
+
