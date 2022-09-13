a5b8a7ab1f8d7775da32b61cf24d3eebb548ccef
Nwajei edgar <edgarlinked@gmail.com>
2022-09-13 12:30:46 -0700

    100

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,5 +22,18 @@ for (ii = 2; ii < nLast; ii++)
 		printf(", ");
 }
 printf("\n");
+
+int i = 1, j = 2, total = 0;
+int k;
+
+while (j < 135301852344706746049)
+{
+	if (j % 2 == 0)
+		total += j;
+	k = j;
+	j += i;
+	i = k;
+}
+printf("%d\n", total);
 return (0);
 }
