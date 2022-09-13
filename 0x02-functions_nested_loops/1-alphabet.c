772a8f568056ccca60a5580ced31c4e1a536d067
Nwajei edgar <edgarlinked@gmail.com>
2022-09-13 05:17:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ *Return: void
+ */
+
+void print_alphabet(void);
+{
+	int sh;
+
+for (sh = 'a'; sh <= 'z'; sh++)
+	_putchar(sh);
+_putchar('\n');
+}
