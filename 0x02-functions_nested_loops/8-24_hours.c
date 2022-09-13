ea0f2d1581d157785dbd304e22cf62404f51f73f
Nwajei edgar <edgarlinked@gmail.com>
2022-09-13 09:02:31 -0700

    Jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,12 @@ for (hr = 0; hr < 24; hr++)
 {
 for (min = 0; min < 60; min++)
 	{
-  _putchar((hr / 10) + '0');
-		_putchar((hr % 10) + '0');
-		_putchar(':');
-		_putchar((min / 10) + '0');
-		_putchar((min % 10) + '0');
-		_putchar('\n');
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
 	}
 
 }
