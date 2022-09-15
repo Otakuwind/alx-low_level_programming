c573cef337f7b2e868f7b038e26bc0cf7d09e50a
Nwajei edgar <edgarlinked@gmail.com>
2022-09-15 07:21:02 -0700

    Print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,17 @@ void print_uint(unsigned int n);
  *
  *
  */
-void print_number(int var)
+void print_number(int n)
 {
-	void print(unsigned int n)
-	{
-		if (n / 10)
-			print(n / 10);
-		_putchar('0' + n % 10);
-	}
+unsigned int num = n;
+if (n < 0)
+{
+	_putchar('-');
+	num = -num;
+}
+if (num > 9)
+{
+	print_number(num / 10);
+}
+_putchar(num % 10 + '0');
+}
