bd512ed336f0469e487e626ddb5bfa433630cbac
Nwajei edgar <edgarlinked@gmail.com>
2022-09-15 04:36:59 -0700

    Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,15 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else
-		{
-			printf("%d ", i)
-		}
-	printf("Buzz\n");
+				else if (i % 5 == 0)
+				printf("Buzz ");
+				else if (i % 3 == 0)
+					printf("Fizz ");
+				else
+				printf("%d ", i)
+					if (i < 100)
+						printf(" ");
+	}
+		printf("Buzz\n");
 	return (0);
 }
