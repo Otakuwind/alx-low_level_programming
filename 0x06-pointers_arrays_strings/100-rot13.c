a6b4d248ad9d80210e52713ff190e20aeeb0c46d
Nwajei edgar <edgarlinked@gmail.com>
2022-09-22 01:44:10 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rot13- encodes a string
- * @s: string
- *
- * Return: string
+ * rot13 - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: decrypted string
  */
-char *rot13(char *s)
+char *rot13(char *n)
 {
-	int a = 0;
+int x, rot_c = 13, i = 0;
+char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
+'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
+'s', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
+'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
+'Z', 'm', 'z'};
 
-	while (s[a])
+while (n[i] != '\0')
+{
+	for (x = 0; x <= 51; x++)
 	{
-		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
-		{if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z' ))
-			{
-				s[a] -= 13;
-				break;
-			}
-
-			s[a] += 13;
-			break;
+		if (n[i] == toswap[x])
+		{
+			n[i] = n[i] + rot_c;
+			x = 51;
 		}
-
-		a==;
+		rot_c = rot_c * -1;
 	}
-
-	return (s);
+	i++;
+}
+return (n);
 }
