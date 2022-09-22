8561ddfb4aadc55c7246531a2b3673afd55b6d21
Nwajei edgar <edgarlinked@gmail.com>
2022-09-22 01:53:43 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,25 +8,20 @@
  */
 char *rot13(char *n)
 {
-int x, rot_c = 13, i = 0;
-char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-'s', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-'Z', 'm', 'z'};
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-while (n[i] != '\0')
+for (i = 0; *(s + i); i++)
 {
-	for (x = 0; x <= 51; x++)
+	for (j = 0; j < 52; j++)
 	{
-		if (n[i] == toswap[x])
+		if (a[j] == *(s + i))
 		{
-			n[i] = n[i] + rot_c;
-			x = 51;
+			*(s + i) = b[j];
+			break;
 		}
-		rot_c = rot_c * -1;
 	}
-	i++;
 }
-return (n);
+return (s);
 }
