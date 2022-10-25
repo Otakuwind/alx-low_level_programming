21646189252e3f1ce651a2d4e5880ca364f51ac7
Nwajei edgar <edgarlinked@gmail.com>
2022-10-25 06:54:32 -0700

    node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,14 +20,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 while (tmp_node)
 	{
-		if (counter + 1 == index)
+if (counter + 1 == index)
 {
-		idx_node = tmp_node->next;
-		if (idx_node)
-		{
-	tmp_node->next = idx_node->next;
-	free(idx_node);
-	return (1);
+idx_node = tmp_node->next;
+if (idx_node)
+{
+tmp_node->next = idx_node->next;
+free(idx_node);
+return (1);
 }
 }
 	tmp_node = tmp_node->next;
